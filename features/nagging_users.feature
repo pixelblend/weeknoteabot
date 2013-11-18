796e2aba13df4c09a32e58b4295e255de8585201
Dan Nuttall <pixelblend@gmail.com>
2013-11-18 14:56:36 +0000

    Add (pending) feature for nag emails

---
+++ features/nagging_users.feature
@@ -0,0 +1,9 @@
+Feature: Nagging users
+
+  # Please see doc/GLOSSARY.mdown for a definition of terms.
+
+  Scenario: Non contributing users are sent a reminder email
+    Given weeknotes have been started
+    And a contributer has submitted
+    When the first nag period ends
+    Then users who have not submitted are reminded to do so
---
+++ features/step_definitions/nag_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a contributer has submitted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the first nag period ends$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^users who have not submitted are reminded to do so$/) do
+  pending # express the regexp above with the code you wish you had
+end
